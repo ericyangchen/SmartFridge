05343244b71f27494d64538cee37cf12471e11bf
ericyangchen <yericchenyang@gmail.com>
2023-06-16 14:45:01 +0800

    chore: add empty notification

--- src/modules/layouts/PopOutReminds.tsx
+++ src/modules/layouts/PopOutReminds.tsx
@@ -13,13 +13,17 @@ export default function PopOutReminds() {
 
   return (
     <Menu>
-      <MenuButton as="button" transition="all 0.2s">
+      <MenuButton as="button" transition="all 0.2s" borderRadius={"full"}>
         <BsFillBellFill size={20} />
       </MenuButton>
       <MenuList w="80vw" float={"right"}>
-        {data?.notification.map((item: string, index: number) => (
-          <MenuItem key={index}>{item}</MenuItem>
-        ))}
+        {data?.notification.length ? (
+          data?.notification.map((item: string, index: number) => (
+            <MenuItem key={index}>{item}</MenuItem>
+          ))
+        ) : (
+          <MenuItem>No notification</MenuItem>
+        )}
       </MenuList>
     </Menu>
   );
